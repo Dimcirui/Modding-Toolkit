aeb5bd325dbaaf12b40c6dba0d0e53e6de0bcab5
Dimcirui <dimcirui@qq.com>
2026-02-28 02:31:35 -0500

    Version update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Modding Toolkit",
     "author": "Dimcirui",
-    "version": (2, 2, 1),
+    "version": (2, 2, 2),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar > MOD Toolkit",
     "description": "Modding Toolkit for Capcom's games",
