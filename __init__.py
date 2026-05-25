deb950eb62fee3fd11e554245613d08ada05ec41
Dimcirui <dimcirui@qq.com>
2026-05-25 19:53:03 +0800

    chore: bump version to 2.5.1.1 for hotfix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Modding Toolkit",
     "author": "Dimcirui",
-    "version": (2, 5, 1),
+    "version": (2, 5, 1, 1),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar > MOD Toolkit",
     "description": "Modding Toolkit for Capcom's games",
